2c3254ec3747fc23298cebf759c39e8ada00b8bd
Aleksandr Ryzhikov <meet.aryzhikov@gmail.com>
2025-09-16 16:12:34 +0700

    feat: Implement CLI argument parser
    This commit adds the parse-args function, which is responsible for
    parsing command-line arguments and converting them into a structured
    command object. This function supports the following commands: help,
    add, list, complete, and remove. If an unknown command is provided, it
    returns a :unknown command.

---
+++ src/cli/parser.lisp
@@ -0,0 +1,23 @@
+(in-package :todo-cli.cli)
+
+(defstruct command
+  "Structure for representing a team"
+  name args options)
+
+(defun parse-args (args)
+  "Parse command line arguments"
+  (cond
+    ((null args)
+      (make-command :name :help))
+    ((string= (first args) "help")
+      (make-command :name :help))
+    ((string= (first args) "add")
+      (make-command :name :add :args (rest args)))
+    ((string= (first args) "list")
+      (make-command :name :list :args (rest args)))
+    ((string= (first args) "complete")
+      (make-command :name :complete :args (rest args)))
+    ((string= (first args) "remove")
+      (make-command :name :remove :args (rest args)))
+    (t
+      (make-command :name :unknown :args args))))
